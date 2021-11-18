4854214790fb3d98c2fe7b0c17354668e9cf756d
xKaosR <94604743+xKaosR@users.noreply.github.com>
2021-11-18 09:30:51 +0000

    Add files via upload

---
+++ p5play_template-main/sketch.js
@@ -0,0 +1,17 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,30,30)
+  box.shapeColor = "gold"
+}
+
+function draw() 
+{
+  background("red");
+drawSprites();
+}
+
+
+
+
+
